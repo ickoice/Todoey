b7e0fa9831da0ba6483eec7f5fb9867664e9290d
Risto Radanliev <ickoice@hotmail.com>
2019-06-06 15:20:57 +0200

    Convert to MVC Design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Risto Radanliev on 6/6/19.
+//  Copyright © 2019 Risto Radanliev. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
